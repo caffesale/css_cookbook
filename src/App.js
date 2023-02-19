09ac52c610500da51a29d60c4abc8eb445d276ff
caffesale <pglibrary80@gmail.com>
2023-02-19 15:22:15 +0900

    Feat: 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route>
+          <div className="App">
+            Hello world!
+          </div>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* remove default spacing */
+/* force styling of type through styling, rather than elements */
+
+* {
+  margin: 0;
+  padding: 0;
+  font: inherit;
+}
+
+/* dark mode user-agent-styles */
+
+html {
+  color-scheme: dark light;
+}
+
+/* min body height */
+
+body {
+  min-height: 100vh;
+}
+
+/* responsive images/videos */
+img,
+picture,
+svg,
+video {
+  display: block;
+  max-width: 100%;
+}
+`
---
+++ src/pages/ImageContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ImageContainer = () => {
+  return (
+    <div>ImageContainer</div>
+  )
+}
+
+export default ImageContainer
